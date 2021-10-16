bad5e39d2ba4a4816c887a671b039be29367a610
abhishekjainmangosciences <abhishek.jain@mangosciences.com>
2021-10-16 23:29:27 +0530

    create react-native app

---
+++ frontend/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
